b0b941ff3a7b5ee9752a1546b8352d5b6d837252
Nicolas Williamson <22387@columbushs.com>
2019-12-10 14:44:33 -0500

    github.com

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,40 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(theDamage){
+        this.health = this.health - theDamage;
+    }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(theDamage){
+        this.health = this.health - theDamage;
+        if(this.health > 0 ){
+            return "NAME has received DAMAGE points of damage"
+        }
+        else if (this.health = 0) {
+            return "NAME has died in act of combat"
+        }
+    }
+
+
+
+
+}
+
+
 
 // Saxon
 class Saxon {}
